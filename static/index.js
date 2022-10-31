78ff0fda9a13a43ac9445326c39a9af752d5dd24
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-10-31 15:58:17 -0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -32,27 +32,5 @@ $(function () {
             }
         });
     });
-    $("#save_button").click(function () {
-        save_data = {
-            "date": new_date,
-            "text": $("#text").val(),
-            "emotion": predicted_emotion
-        }
-        $.ajax({
-            type: 'POST',
-            url: "/save-entry",
-            data: JSON.stringify(save_data),
-            dataType: "json",
-            contentType: 'application/json',
-            success: function () {
-                alert("Sua entrada foi salva com sucesso!")
-                window.location.reload()
-            },
-            error: function (result) {
-                alert(result.responseJSON.message)
-            }
-        });
-
-    });
 })
 
